7b15be417befe93723f2c9bac1cc9959adbe496d
orznmi <ornaoza@gmail.com>
2020-11-10 19:23:25 +0700

    GD VueCLI Webpack

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+
+Vue.use(Vuetify)
+
+const opts = {}
+
+export default new Vuetify(opts)
+
+//icon
+Vue.use(Vuetify, {
+    iconfont: 'mdi'
+  })
